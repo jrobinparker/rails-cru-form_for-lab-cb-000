c2cc73c88dfdd71f70b0e6f667ba87b050c2e7fe
Jeremy Parker <jrobinparker@gmail.com>
2018-06-20 16:39:06 +0000

    automatically backed up by learn

--- app/controllers/genre_controller.rb
+++ app/controllers/genre_controller.rb
@@ -1,2 +1,15 @@
 class GenreController < ApplicationController
+  
+  def show 
+    @genre = Genre.find(params[:id])
+  end 
+  
+  def new 
+    @genre = Genre.new 
+  end 
+  
+  def create 
+    @genre = Genre.new
+    
+  
 end
