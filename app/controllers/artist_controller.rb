23e16ed7879f8c9a7ccec955c8891369f45629de
Jeremy Parker <jrobinparker@gmail.com>
2018-06-20 16:25:08 +0000

    automatically backed up by learn

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -1,2 +1,20 @@
 class ArtistController < ApplicationController
+  
+  def show
+    @artist = Artist.find(params[:id])
+  end 
+  
+  def new 
+  end 
+  
+  def create 
+  end 
+  
+  def edit 
+  end 
+
+  def update 
+  end 
+  
+  
 end
